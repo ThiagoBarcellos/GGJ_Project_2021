009e599c39149bd20473e2e4c4be2aabf97554ba
Thiago Barcellos Mattos <th.mattos04@gmail.com>
2021-01-30 13:10:50 -0300

    (update)
    Updated bat enemy attack

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;  
+﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
@@ -6,18 +7,19 @@ public class EnemyAI : MonoBehaviour
     public bool isTouching = false;
     public bool attacked = false;
     public bool onSpawn = true;
-    public int movementidth = 5;
+    public float movementidth = 5f;
     public GameObject startingPosition;
-
     public float speed = 0.8f;
 
-    void Start(){
+    void Start()
+    {
         Player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    private void FixedUpdate(){
-        if (onSpawn)
-            IdleMovement();
+    private void FixedUpdate()
+    {
+        //if (onSpawn)
+           // IdleMovement();
 
         if (isTouching && !attacked)
             MoveTo(Player);
@@ -27,7 +29,8 @@ public class EnemyAI : MonoBehaviour
             MoveTo(startingPosition);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision){
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
         if (collision.gameObject.CompareTag("Player") && attacked == false)
         {
             isTouching = true;
@@ -38,24 +41,34 @@ public class EnemyAI : MonoBehaviour
             onSpawn = true;
         }
     }
-    private void OnTriggerExit2D(Collider2D collision){
-        if (collision.gameObject.CompareTag("Player") && attacked == false){
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && attacked == false)
+        {
             isTouching = false;
             attacked = false;
+            GameObject.FindGameObjectWithTag("DashLimit").GetComponent<CircleCollider2D>().enabled = true;
         }
     }
 
-    void MoveTo(GameObject target){
+    void MoveTo(GameObject target)
+    {
         Vector2 toTarget = target.transform.position - transform.position;
-
         transform.Translate(toTarget * speed * Time.deltaTime);
     }
 
-    void IdleMovement()
+    /*void IdleMovement()
     {
-        if (this.transform.position.x < startingPosition.transform.position.x + movementidth)
-            transform.Translate(Vector2.right * 0.5f * Time.deltaTime);
-        else if (this.transform.position.x > startingPosition.transform.position.x - movementidth)
-            transform.Translate(Vector2.left * 0.5f * Time.deltaTime);
-    }
+        float direction = 1f;
+        Vector2 localPosition = this.transform.localPosition;
+        Vector2 initialPosition = startingPosition.transform.position;
+
+        if (localPosition.x < initialPosition.x + movementidth && localPosition.x > initialPosition.x - movementidth)
+        {
+            transform.Translate(Vector2.right * 0.5f * direction * Time.fixedDeltaTime);
+
+            if (localPosition.x >= initialPosition.x - movementidth || localPosition.x <= initialPosition.x + movementidth)
+                direction *= -1;
+        }
+    }*/
 }
