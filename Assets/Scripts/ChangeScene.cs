0fadf0fce01f5dbc7aa66110c46dfec255d1f670
Evelyn Correa <evelynfcorrea@gmail.com>
2021-01-30 10:22:22 -0300

    ChangeScene, prefabs, initalScene

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    private int scenes, currentScene;
+    void Start() {
+        scenes = SceneManager.sceneCountInBuildSettings;
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.tag == "Player")
+            SceneManager.LoadScene(currentScene + 1, LoadSceneMode.Single);
+    }
+}
